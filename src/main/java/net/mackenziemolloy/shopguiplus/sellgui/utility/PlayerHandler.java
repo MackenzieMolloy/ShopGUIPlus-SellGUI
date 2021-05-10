3d9a408eda0e21e86c2202a7d31c5c22dafd23c7
SirBlobman <blobmansrealm@gmail.com>
2021-05-10 19:27:28 -0400

    Optimize  sound code.

--- src/main/java/net/mackenziemolloy/shopguiplus/sellgui/utility/PlayerHandler.java
+++ src/main/java/net/mackenziemolloy/shopguiplus/sellgui/utility/PlayerHandler.java
@@ -1,111 +1,52 @@
 package net.mackenziemolloy.shopguiplus.sellgui.utility;
 
+import java.util.Locale;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import net.mackenziemolloy.shopguiplus.sellgui.SellGUI;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Sound;
-import org.bukkit.entity.Player;
-
 public class PlayerHandler {
+    private static Sound getSound(String event) throws IllegalArgumentException {
+        SellGUI plugin = JavaPlugin.getPlugin(SellGUI.class);
+        String soundPath = String.format(Locale.US, "options.sounds.events.%s", event);
 
-    private final SellGUI sellGUI;
-
-    public PlayerHandler(final SellGUI sellGUI) {
-        this.sellGUI = sellGUI;
+        String soundValue = plugin.configFile.getString(soundPath);
+        return Sound.valueOf(soundValue);
     }
 
-    public static boolean playSound(Player player, String event) {
-
+    public static void playSound(Player player, String event) {
         SellGUI sellGUI = SellGUI.getInstance();
+        if(!sellGUI.configFile.getBoolean("options.sounds.enabled")) return;
 
-        boolean enabled = sellGUI.configFile.getBoolean("options.sounds.enabled");
-        if(enabled) {
+        float volume = 1.0F;
+        float pitch = 1.0F;
 
-            double volume = 1.0;
-            double pitch = 1.0;
-
-            if(sellGUI.configFile.isDouble("options.sounds.pitch") || sellGUI.configFile.isInt("options.sounds.pitch")) {
-
-                pitch = Double.valueOf(sellGUI.configFile.getString("options.sounds.pitch"));
-
-            }
-
-            if(sellGUI.configFile.isDouble("options.sounds.volume") || sellGUI.configFile.isInt("options.sounds.volume")) {
-
-                volume = Double.valueOf(sellGUI.configFile.getString("options.sounds.volume"));
-
-            }
-
-            if(event.equalsIgnoreCase("open")) {
-
-                try {
-
-                    player.playSound(player.getLocation(), Sound.valueOf(sellGUI.configFile.getString("options.sounds.events.open")), (float) volume, (float) pitch);
-
-                }
-
-                catch(Exception e) {
-
-                    if(sellGUI.configFile.getBoolean("options.sounds.error_notifcation")) {
-
-                        sellGUI.getServer().getConsoleSender().sendMessage(ChatColor.DARK_RED + "[ShopGUIPlus-SellGUI] Invalid Sound for version " + sellGUI.version + " => '" + sellGUI.configFile.getString("options.sounds.events.open") + "' (open)");
-
-                    }
-
-                }
-
-            }
-
-            else if(event.equalsIgnoreCase("success")) {
-
-                try {
-
-                    player.playSound(player.getLocation(), Sound.valueOf(sellGUI.configFile.getString("options.sounds.events.success")), (float) volume, (float) pitch);
-
-                }
-
-                catch(Exception e) {
-
-                    if(sellGUI.configFile.getBoolean("options.sounds.error_notifcation")) {
-
-                        sellGUI.getServer().getConsoleSender().sendMessage(ChatColor.DARK_RED + "[ShopGUIPlus-SellGUI] Invalid Sound for version " + sellGUI.version + " => '" + sellGUI.configFile.getString("options.sounds.events.success") + "' (success)");
-
-                    }
-
-                }
-
-            }
-
-            else if(event.equalsIgnoreCase("failed")) {
-
-                try {
-
-                    player.playSound(player.getLocation(), Sound.valueOf(sellGUI.configFile.getString("options.sounds.events.failed")), (float) volume, (float) pitch);
-
-                }
-
-                catch(Exception e) {
-
-                    if(sellGUI.configFile.getBoolean("options.sounds.error_notifcation")) {
-
-                        sellGUI.getServer().getConsoleSender().sendMessage(ChatColor.DARK_RED + "[ShopGUIPlus-SellGUI] Invalid Sound for version " + sellGUI.version + " => '" + sellGUI.configFile.getString("options.sounds.events.failed") + "' (failed)");
-
-                    }
-
-                }
-
-            }
-
+        if(sellGUI.configFile.isDouble("options.sounds.pitch") || sellGUI.configFile.isInt("options.sounds.pitch")) {
+            pitch = (float) sellGUI.configFile.getDouble("options.sounds.pitch");
         }
 
-        else {
-
-            return true;
-
+        if(sellGUI.configFile.isDouble("options.sounds.volume") || sellGUI.configFile.isInt("options.sounds.volume")) {
+            volume = (float) sellGUI.configFile.getDouble("options.sounds.volume");
         }
 
-        return true;
-
+        try {
+            Location location = player.getLocation();
+            Sound sound = getSound(event);
+            player.playSound(location, sound, volume, pitch);
+        } catch(Exception ex) {
+            if(sellGUI.configFile.getBoolean("options.sounds.error_notifcation")) {
+                CommandSender console = Bukkit.getConsoleSender();
+                console.sendMessage(ChatColor.DARK_RED + "[ShopGUIPlus-SellGUI] Invalid Sound for version "
+                        + sellGUI.version + " => '" + sellGUI.configFile.getString("options.sounds.events." + event)
+                        + "' (failed)");
+            }
+        }
     }
-
 }
