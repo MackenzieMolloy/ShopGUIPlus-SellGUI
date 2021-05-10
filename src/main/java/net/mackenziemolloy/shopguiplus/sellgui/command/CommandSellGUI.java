5196088b314cc16ea2edd93362eb7b201cd9e02f
SirBlobman <blobmansrealm@gmail.com>
2021-05-10 19:51:07 -0400

    Optimize sell command a bit more.

--- src/main/java/net/mackenziemolloy/shopguiplus/sellgui/command/CommandSellGUI.java
+++ src/main/java/net/mackenziemolloy/shopguiplus/sellgui/command/CommandSellGUI.java
@@ -20,9 +20,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.command.TabExecutor;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
@@ -196,142 +198,100 @@ public final class CommandSellGUI implements TabExecutor {
         Gui gui = new Gui(guiSize, sellGUITitle);
         PlayerHandler.playSound(player, "open");
 
-        List<Integer> ignoredSlots = new ArrayList<>();
+        List<Integer> ignoredSlotList = new ArrayList<>();
 
-        if(!(this.plugin.configFile.getConfigurationSection("options.decorations") == null) && this.plugin.configFile.getConfigurationSection("options.decorations").getKeys(false).size() > 0) {
-            for (int i = 0; i < this.plugin.configFile.getConfigurationSection("options.decorations").getKeys(false).size(); i++) {
+        ConfigurationSection sectionOptionsDecorations = this.plugin.configFile.getConfigurationSection("options.decorations");
+        if(!(sectionOptionsDecorations == null) && sectionOptionsDecorations.getKeys(false).size() > 0) {
+            for (int i = 0; i < sectionOptionsDecorations.getKeys(false).size(); i++) {
+                Object[] processItem = sectionOptionsDecorations.getKeys(false).toArray();
 
-                @Nullable Object[] ProcessItem = this.plugin.configFile.getConfigurationSection("options.decorations").getKeys(false).toArray();
-                ConfigurationSection decorations = this.plugin.configFile.getConfigurationSection("options.decorations");
-
-                if (decorations.getString(ProcessItem[i] + ".item.material") == null ||
-                        Material.matchMaterial(decorations.getString(ProcessItem[i] + ".item.material")) == null ||
-                        decorations.getString(ProcessItem[i] + ".slot") == null ||
-                        decorations.getInt(ProcessItem[i] + ".slot") > (guiSize * 9) - 1 ||
-                        decorations.getInt(ProcessItem[i] + ".slot") < 0) {
-
-                    this.plugin.getLogger().info("Error loading decoration item identified as " + ProcessItem[i]);
+                String materialName = sectionOptionsDecorations.getString(processItem[i] + ".item.material");
+                Material material;
+                if (materialName == null || (material = Material.matchMaterial(materialName)) == null ||
+                        sectionOptionsDecorations.getString(processItem[i] + ".slot") == null ||
+                        sectionOptionsDecorations.getInt(processItem[i] + ".slot") > (guiSize * 9) - 1 ||
+                        sectionOptionsDecorations.getInt(processItem[i] + ".slot") < 0) {
+                    this.plugin.getLogger().info("Error loading decoration item identified as " + processItem[i]);
                     continue;
 
                 }
 
-                ItemStack toAdd = new ItemStack(Material.matchMaterial(decorations.getString(ProcessItem[i] + ".item.material")));
+                ItemStack toAdd = new ItemStack(material);
 
-                if(!(String.valueOf(decorations.getInt(ProcessItem[i] + ".item.damage")) == null)) {
-                    toAdd.setDurability((short) decorations.getInt(ProcessItem[i] + ".item.damage"));
+                int durability = sectionOptionsDecorations.getInt(processItem[i] + ".item.damage", 0);
+                if(durability != 0) {
+                    setDurability(toAdd, serverVersion, durability);
                 }
 
-                if (String.valueOf(decorations.get(ProcessItem[i] + ".item.quantity")) != "null") {
-                    toAdd.setAmount(decorations.getInt(ProcessItem[i] + ".item.quantity"));
-                }
+                int quantity = sectionOptionsDecorations.getInt(processItem[i] + ".item.quantity", 1);
+                toAdd.setAmount(quantity);
 
-                ItemMeta im = toAdd.getItemMeta();
-
-                if (!(decorations.getString(ProcessItem[i] + ".item.name") == null)) {
-                    String ItemName = ChatColor.translateAlternateColorCodes('&',
-                            String.valueOf(decorations.getString(ProcessItem[i] + ".item.name")));
-
-                    im.setDisplayName(ItemName);
-                }
-
-                if(!(String.valueOf(decorations.getList(ProcessItem[i] + ".item.lore")) == null)) {
-
-                    List<String> loreToSet = new ArrayList<>();
-
-                    for(int ii = 0; ii < decorations.getStringList(ProcessItem[i] + ".item.lore").size(); ii++) {
-
-                        List<String> loreLines = decorations.getStringList(ProcessItem[i] + ".item.lore");
-
-                        String loreLineToAdd = ChatColor.translateAlternateColorCodes('&', loreLines.get(ii));
-
-                        loreToSet.add(loreLineToAdd);
-
+                ItemMeta toAddMeta = toAdd.getItemMeta();
+                if(toAddMeta != null) {
+                    String displayName = sectionOptionsDecorations.getString(processItem[i] + ".item.name");
+                    if (displayName != null) {
+                        String ItemName = ChatColor.translateAlternateColorCodes('&', displayName);
+                        toAddMeta.setDisplayName(ItemName);
                     }
 
-                    im.setLore(loreToSet);
+                    List<String> loreList = sectionOptionsDecorations.getStringList(processItem[i] + ".item.lore");
+                    if(!loreList.isEmpty()) {
+                        List<String> loreToSet = new ArrayList<>();
+                        for(int ii = 0; ii < sectionOptionsDecorations.getStringList(processItem[i] + ".item.lore").size(); ii++) {
+                            List<String> loreLines = sectionOptionsDecorations.getStringList(processItem[i] + ".item.lore");
+                            String loreLineToAdd = ChatColor.translateAlternateColorCodes('&', loreLines.get(ii));
+                            loreToSet.add(loreLineToAdd);
 
-                }
+                        }
 
-                List<String> consoleCommands = new ArrayList<>();
-
-                if (!(String.valueOf(decorations.getList(ProcessItem[i] + ".commandsOnClickConsole")) == null)) {
-
-                    for(int iii = 0; iii < decorations.getStringList(ProcessItem[i] + ".commandsOnClickConsole").size(); iii++) {
-
-                        consoleCommands.add(decorations.getStringList(ProcessItem[i] + ".commandsOnClickConsole").get(iii));
-
+                        toAddMeta.setLore(loreToSet);
                     }
-
                 }
 
-                List<String> playerCommands = new ArrayList<>();
-
-                if (!(String.valueOf(decorations.getList(ProcessItem[i] + ".commandsOnClick")) == null)) {
-
-                    for(int iii = 0; iii < decorations.getStringList(ProcessItem[i] + ".commandsOnClick").size(); iii++) {
-
-                        playerCommands.add(decorations.getStringList(ProcessItem[i] + ".commandsOnClick").get(iii));
-
-                    }
-
-                }
-
-                toAdd.setItemMeta(im);
+                List<String> consoleCommandList = sectionOptionsDecorations.getStringList(processItem[i] + ".commandsOnClickConsole");
+                List<String> playerCommandList = sectionOptionsDecorations.getStringList(processItem[i] + ".commandsOnClick");
+                toAdd.setItemMeta(toAddMeta);
 
                 GuiItem guiItem = new GuiItem(toAdd, event -> {
                     event.setCancelled(true);
+                    HumanEntity human = event.getWhoClicked();
+                    CommandSender console = Bukkit.getConsoleSender();
+                    String humanName = human.getName();
 
-                    for (String consoleCommand : consoleCommands) {
-
-                        this.plugin.getServer().dispatchCommand(this.plugin.getServer().getConsoleSender(), consoleCommand.replace("%PLAYER%", event.getWhoClicked().getName()));
-
+                    for (String consoleCommand : consoleCommandList) {
+                        Bukkit.dispatchCommand(console, consoleCommand.replace("%PLAYER%", humanName));
                     }
 
-                    for (String playerCommand : playerCommands) {
-
-                        this.plugin.getServer().dispatchCommand(event.getWhoClicked(), playerCommand.replace("%PLAYER%", event.getWhoClicked().getName()));
-
+                    for (String playerCommand : playerCommandList) {
+                        Bukkit.dispatchCommand(human, playerCommand.replace("%PLAYER%", humanName));
                     }
-
                 });
 
-                gui.setItem(decorations.getInt(ProcessItem[i] + ".slot"), guiItem);
-
-                ignoredSlots.add(decorations.getInt(ProcessItem[i] + ".slot"));
-
+                gui.setItem(sectionOptionsDecorations.getInt(processItem[i] + ".slot"), guiItem);
+                ignoredSlotList.add(sectionOptionsDecorations.getInt(processItem[i] + ".slot"));
             }
         }
 
         gui.setCloseGuiAction(event -> {
-
-            final Map<ItemStack, Map<Short, Integer>> soldMap2 = new HashMap<>();
-
+            Map<ItemStack, Map<Short, Integer>> soldMap2 = new HashMap<>();
             Map<EconomyType, Double> moneyMap = new EnumMap<>(EconomyType.class);
 
             double totalPrice = 0;
             int itemAmount = 0;
-
-            Inventory items = event.getInventory();
-            final Boolean[] ExcessItems = {false};
-
+            boolean[] excessItems = {false};
             boolean itemsPlacedInGui = false;
 
             Inventory inventory = event.getInventory();
             for (int a = 0; a < inventory.getSize(); a++) {
                 ItemStack i = inventory.getItem(a);
-
                 if (i == null) continue;
 
-                if(ignoredSlots.contains(a)) {
-
+                if(ignoredSlotList.contains(a)) {
                     continue;
-
                 }
 
                 itemsPlacedInGui = true;
-
                 if (ShopHandler.getItemSellPrice(i, player) > 0) {
-
                     itemAmount += i.getAmount();
 
                     @Deprecated
@@ -359,40 +319,27 @@ public final class CommandSellGUI implements TabExecutor {
                     moneyMap.put(itemEconomyType, amountSold2);
 
 
-                }
-                else {
-
-                    final Map<Integer, ItemStack> fallenItems = event.getPlayer().getInventory().addItem(i);
+                } else {
+                    Map<Integer, ItemStack> fallenItems = event.getPlayer().getInventory().addItem(i);
                     fallenItems.values().forEach(item -> {
                         event.getPlayer().getWorld().dropItemNaturally(event.getPlayer().getLocation().add(0, 0.5, 0), item);
-                        ExcessItems[0] = true;
+                        excessItems[0] = true;
                     });
 
                 }
 
             }
 
-            if(ExcessItems[0]) {
-
-                String ExcessItemsMsg = ChatColor.translateAlternateColorCodes('&', this.plugin.configFile.getString("messages.inventory_full"));
-                player.sendMessage(ExcessItemsMsg);
-
+            if(excessItems[0]) {
+                sendMessage(player, "inventory_full");
             }
 
             if (totalPrice > 0) {
-
                 PlayerHandler.playSound((Player) event.getPlayer(), "success");
-
                 StringBuilder formattedPricing = new StringBuilder();
-
                 for(Map.Entry<EconomyType, Double> entry : moneyMap.entrySet()) {
-                    EconomyProvider economyProvider =
-                            ShopGuiPlusApi.getPlugin().getEconomyManager().getEconomyProvider(entry.getKey());
-                    economyProvider
-                            .deposit(player, entry.getValue());
-
-
-
+                    EconomyProvider economyProvider = ShopGuiPlusApi.getPlugin().getEconomyManager().getEconomyProvider(entry.getKey());
+                    economyProvider.deposit(player, entry.getValue());
                     formattedPricing.append(economyProvider.getCurrencyPrefix()).append(ShopHandler.getFormattedPrice(entry.getValue(), entry.getKey())).append(economyProvider.getCurrencySuffix()).append(", ");
 
                 }
@@ -406,7 +353,6 @@ public final class CommandSellGUI implements TabExecutor {
 
 
                 if(this.plugin.configFile.getInt("options.receipt_type") == 1 || this.plugin.configFile.getString("messages.items_sold").contains("{list}")) {
-
                     for(Map.Entry<ItemStack, Map<Short, Integer>> entry : soldMap2.entrySet()) {
                         for(Map.Entry<Short, Integer> damageEntry : entry.getValue().entrySet()) {
                             @Deprecated
@@ -434,13 +380,9 @@ public final class CommandSellGUI implements TabExecutor {
 
                         }
                     }
-
                 }
 
                 if(this.plugin.configFile.getInt("options.receipt_type") == 1) {
-
-
-
                     String msg = ChatColor.translateAlternateColorCodes('&', this.plugin.configFile.getString("messages.items_sold").replace("{earning}", formattedPricing).replace("{receipt}", "").replace("{list}", itemList.substring(0, itemList.length()-2)).replace("{amount}", String.valueOf(itemAmount)));
                     String receiptName = ChatColor.translateAlternateColorCodes('&', this.plugin.configFile.getString("messages.receipt_text"));
 
@@ -456,7 +398,6 @@ public final class CommandSellGUI implements TabExecutor {
                 }
 
                 else {
-
                     String msg = ChatColor.translateAlternateColorCodes('&', this.plugin.configFile.getString("messages.items_sold")
                             .replace("{earning}", formattedPricing)
                             .replace("{receipt}", "")
@@ -468,13 +409,9 @@ public final class CommandSellGUI implements TabExecutor {
                 }
 
                 if(this.plugin.configFile.getBoolean("options.sell_titles")) {
-
-
                     @Nullable String sellTitle = ChatColor.translateAlternateColorCodes('&', this.plugin.configFile.getString("messages.sell_title").replace("{earning}", formattedPricing).replace("{amount}", String.valueOf(itemAmount)));
                     @Nullable String sellSubtitle = ChatColor.translateAlternateColorCodes('&', this.plugin.configFile.getString("messages.sell_subtitle").replace("{earning}", formattedPricing).replace("{amount}", String.valueOf(itemAmount)));
-
                     player.sendTitle(sellTitle, sellSubtitle);
-
                 }
 
                 if(this.plugin.configFile.getBoolean("options.action_bar_msgs")) {
@@ -482,7 +419,6 @@ public final class CommandSellGUI implements TabExecutor {
                         String actionBarMessage = ChatColor.translateAlternateColorCodes('&', this.plugin.configFile.getString("messages.action_bar_items_sold").replace("{earning}", formattedPricing).replace("{amount}", String.valueOf(itemAmount)));
                         player.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(actionBarMessage));
                     }
-
                 }
 
             } else {
@@ -514,5 +450,19 @@ public final class CommandSellGUI implements TabExecutor {
         long maxMemoryMB = (maxMemory / 1_048_576L);
 
         return String.format(Locale.US, "%s / %s MiB", usedMemoryMB, maxMemoryMB);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void setDurability(ItemStack item, int version, int durability) {
+        if(version < 13) {
+            item.setDurability((short) durability);
+            return;
+        }
+
+        ItemMeta meta = item.getItemMeta();
+        if(meta instanceof Damageable) {
+            ((Damageable) meta).setDamage(durability);
+            item.setItemMeta(meta);
+        }
     }
 }
